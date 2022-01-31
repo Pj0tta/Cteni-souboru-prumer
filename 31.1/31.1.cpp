8dbf4520a5cdbb1c8fce0181351d15028900b9a0
Petr Habel <petr.habel.020@pslib.cz>
2022-01-31 13:49:20 +0100

    Přidejte soubory projektu.

---
+++ 31.1/31.1.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	FILE* vstup = fopen("Znamky.txt", "r");
+	if (vstup == NULL) {
+		printf("ChYbOvAl Jsi: Dopustil jste se argumentacniho faulu priteli :/");
+		return 1;
+	}
+	while (!feof(vstup)) {
+		float prumer = 0;
+		char jmeno[20];
+		int znamky;
+		fscanf(vstup, "%s", &jmeno);
+		for (int i = 0; i <= 10; i++) {
+			fscanf(vstup, "%d", &znamky);
+			prumer+=znamky;
+		}
+		prumer /= 10;
+		printf("%s: %7.2f\n", jmeno, prumer);
+
+
+	}
+}
+
